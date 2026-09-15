b7811b2eb174c0b5490486f81d5ad72fd0a62565
Nikee <ciahsariah92@gmail.com>
2026-09-15 13:28:05 +0700

    Add files via upload

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,2 @@
-const CACHE='wta-shell-v2';
-self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(['/','/sw.js'])))});
-self.addEventListener('activate',e=>e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET')return;e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)))});
+self.addEventListener("install", e=>self.skipWaiting());
+self.addEventListener("activate", e=>e.waitUntil(self.clients.claim()));
